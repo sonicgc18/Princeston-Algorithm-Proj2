9aa2ac9a61c54ec268f901781bfa786a5b2a1b5a
sonicgc18 <Sonicgc@ChengdeMacBook-Pro.local>
2016-11-03 00:18:55 -0700

    commit test1

--- src/Deque.java
+++ src/Deque.java
@@ -1,6 +1,9 @@
 
 public class Deque<Item> implements Iterable<Item> {
    public Deque()                           // construct an empty deque
+   {
+	   
+   }
    public boolean isEmpty()                 // is the deque empty?
    public int size()                        // return the number of items on the deque
    public void addFirst(Item item)          // add the item to the front
